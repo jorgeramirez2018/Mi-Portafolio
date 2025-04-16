a72b82884e65b89b96d8eb6e4ad2ac56a9896ee8
jorgeramirez2018 <jorgeramirez2015.jr@gmail.com>
2025-04-16 17:42:33 -0500

    Actualizacion

---
+++ script.js/script.js
@@ -0,0 +1,25 @@
+function emailJs(form){
+    const formEl = document.querySelector(".form")
+    formEl.addEventListener("submit", async (input) => {
+        input.preventDefault()
+
+        const request = {
+            nombre: input.target.nombre.value,
+            email: input.target.email.value,
+            numero: input.target.numero.value,
+            asunto: input.target.asunto.value,
+            mensaje: input.target.mensaje.value
+        }
+
+        var serviceID = "service_egk121w";
+        var templateID = "template_hf9phkg";
+        try{
+            await emailjs.send(serviceID, templateID, request)
+            alert("Gracias por contactarme")
+        }catch(err){
+            console.log(err)
+        }
+    })
+}
+
+emailJs()
